2aad2387dcc4ed8828cf47a7f8f048f61b02e80c
Elora <chibi.elora@gmail.com>
2024-03-27 22:22:18 -0400

    changes to travel page

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json' , 'utf8'));
+
 /* GET travel view */
 
 const travel = (req, res) => {
-    res.render('travel', { title: "Travlr Getaways"});
+    res.render('travel', { title: "Travlr Getaways" , trips});
 
 };
 
