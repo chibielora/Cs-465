e244f199702dd6a12c462c447b6288ec8d8b9139
Elora <chibi.elora@gmail.com>
2024-03-31 02:16:46 -0400

    Database debugging

--- travlr/app_server/models/seed.js
+++ travlr/app_server/models/seed.js
@@ -1,12 +1,12 @@
 // Bring in the DB connection and the Trip schematic
 
-const Mongoose = require('./db');
-const Trip = require('./travlr');
+const Mongoose = require("./db");
+const Trip = require("./travlr");
 
 // Read seed data from json file
 
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'))
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"))
 
 // delete any existing records, then insert seed data
 
@@ -14,13 +14,12 @@ const seedDB = async () => {
 
     await Trip.deleteMany({});
     await Trip.insertMany(trips);
-
 };
 
 // Close the MongoDB connection and exit
 
 seedDB().then(async () => {
-
+    console.log("after Connection Established")
     await Mongoose.connection.close();
     process.exit(0);
 
