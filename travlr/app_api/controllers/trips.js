f7f207e07370b5b83b1f5277d3e90a970c35f652
Elora <chibi.elora@gmail.com>
2024-04-07 21:14:19 -0400

    Added findByCode to find trips

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -27,6 +27,32 @@ const tripslist = async(req, res) => {
     }
 };
 
+// GET: /trips/:tripcode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+
+const tripsFindByCode = async(req, res) => {
+    const q = await Model
+        .find({ 'code' : req.params.tripCode }) // Return single record
+        .exec();
+
+    // Uncomment the following line to show results of query on the console
+    // console.log(q);
+
+    if(!q) {
+        // Database returned no data
+        return res
+            .status(404)
+            .json(err);
+    } else {
+        // Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
 module.exports = {
-    tripslist
+    tripslist,
+    tripsFindByCode
 };
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -5,8 +5,16 @@ const router = express.Router(); // Router logic
 const tripsController = require('../controllers/trips');
 
 // Define route for our trips' endpoint
+// router.get('/trips', tripsController.tripslist);
+
+ // GET Method routes tripsList
 router
-    .route('trips')
-    .get(tripsController.tripslist); // GET Method routes tripsList
+    .route('/trips')
+    .get(tripsController.tripslist);
+
+ // GET Method routes tripsFindByCode - require parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode);
 
 module.exports = router;
