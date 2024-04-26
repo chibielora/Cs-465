556e38d94289183a3405347667312febc0a2705d
Elora <chibi.elora@gmail.com>
2024-04-26 00:29:12 -0400

    Set up backend authentication

--- travlr/app.js
+++ travlr/app.js
@@ -1,8 +1,12 @@
+// Initialize passport authentication
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport')
 
 // Define Server Routers
 var indexRouter = require('./app_server/routes/index');
@@ -14,6 +18,9 @@ var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
+
+// Authentication
+require('./app_api/config/passport');
 
 // Bring in the database
 require('./app_api/models/db');
@@ -33,11 +40,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize);
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -48,6 +56,17 @@ app.use('/travel', travelRouter);
 app.use('/rooms', roomsRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({
+        "message": err.name + ": " + err.message
+      });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
